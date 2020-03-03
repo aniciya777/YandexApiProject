8f99528c9b54419fb1b1659204b1801a8eb6487c
Aleksandr <sanekclubstr@mail.ru>
2020-03-03 16:50:27 +0500

    2) Add actions on clicks PageUp and PageDown as change scale of map

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import requests
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import Qt
 
 SCREEN_SIZE = [600, 450]
 
@@ -13,9 +14,18 @@ class Example(QWidget):
         super().__init__()
         self.coords = 37.530887, 55.703118
         self.scale = 0.002
+        self.scale_coeff = 2
         self.map_style = 'map'
         self.initUI()
         self.getImage()
+
+    def initUI(self):
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.pixmap = QPixmap()
 
     def getImage(self):
         map_request = "http://static-maps.yandex.ru/1.x/"
@@ -29,17 +39,16 @@ class Example(QWidget):
             print(map_request)
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
-        ## Изображение
-        self.pixmap = QPixmap()
         self.pixmap.loadFromData(response.content)
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
         self.image.setPixmap(self.pixmap)
 
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageDown:
+            self.scale *= self.scale_coeff
+            self.getImage()
+        elif event.key() == Qt.Key_PageUp:
+            self.scale /= self.scale_coeff
+            self.getImage()
 
 
 if __name__ == '__main__':
